1fd244dea01495d613dcc434587400f1c19ff006
Hendrik <hendrik@hr94.de>
2021-03-25 16:04:01 +0100

    add WebAPIClient

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        private static readonly HttpClient cli = new HttpClient();
+
+        static async Task Main(string[] args)
+        {
+            await ProcessRepositories();
+        }
+
+        private static async Task ProcessRepositories()
+        {
+            cli.DefaultRequestHeaders.Accept.Clear();
+            cli.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")
+            );
+            cli.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = cli.GetStreamAsync("https://api.github.com/users/isotop7/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+            foreach (var repo in repositories)
+            {
+                Console.WriteLine(repo.name);
+            }
+        }
+    }
+}
